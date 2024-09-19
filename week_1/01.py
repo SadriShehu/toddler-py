19c430a5e9960760230324a3d3dc92606b7323e3
sadrishehu <sadrishehu@gmail.com>
2024-09-19 20:15:28 +0200

    week-1: variablat

---
+++ week_1/01.py
@@ -0,0 +1,41 @@
+# Variabla e tipit string
+varString = 'Sadri'
+print(varString)
+print(type(varString))
+
+# Variabla e tipit int
+varInt = 7
+print(varInt)
+print(type(varInt))
+
+# Variabla e tipit bool
+varBool = True
+print(varBool)
+print(type(varBool))
+
+# Variabla e tipit float
+varFloat = 1.1
+print(varFloat)
+print(type(varFloat))
+
+# Variabla e tipit tuple
+varTuple = (1,2,3)
+print(varTuple)
+print(type(varTuple))
+
+# Variabla e tipit list
+varList = ["Sadri", 1, True, [1,2,3], (1,2,3)]
+print(varList)
+print(type(varList))
+
+# Variabla e tipit dict
+varDict = {
+    "key": "value",
+    1: (1,2,3),
+    "list": [1,2,3],
+    "dict": {
+        1: 2
+    }
+}
+print(varDict)
+print(type(varDict))
